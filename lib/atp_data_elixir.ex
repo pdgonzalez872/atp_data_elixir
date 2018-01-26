581bf3bac3a774203f25bda7f036ae38246480da
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-01-26 12:27:07 -0500

    Create mix project

---
+++ lib/atp_data_elixir.ex
@@ -0,0 +1,18 @@
+defmodule AtpDataElixir do
+  @moduledoc """
+  Documentation for AtpDataElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AtpDataElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/atp_data_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule AtpDataElixirTest do
+  use ExUnit.Case
+  doctest AtpDataElixir
+
+  test "greets the world" do
+    assert AtpDataElixir.hello() == :world
+  end
+end
