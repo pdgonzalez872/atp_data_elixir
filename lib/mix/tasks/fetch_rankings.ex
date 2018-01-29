884fb04d4f6d54d1bba2b2996ad1977daf3521fc
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-01-29 17:21:05 -0600

    Add comments.

--- lib/mix/tasks/fetch_rankings.ex
+++ lib/mix/tasks/fetch_rankings.ex
@@ -1,10 +1,14 @@
 defmodule Mix.Tasks.FetchRankings do
   use Mix.Task
 
-  def run(_) do
-    IO.puts "Starting"
+  require Logger
 
+  def run(_) do
+    Logger.info "Starting"
+
+    ############################################
     # Do time elapsed
+
 
     # request HTTPoison
 
@@ -15,8 +19,12 @@ defmodule Mix.Tasks.FetchRankings do
     # Do something with that parsed body -> save it to files? in a list in memory?
 
     # Discuss strategy with Rapha ->
-    #  One entity that distributes the elements in the list to other entities (a url to send a request to, )
+    # I will have a list with many urls. This is what I have in mind:
+    #  - One entity that distributes the elements in the list to other entities (a url, string) - A Supervisor?
+    #  - each process that gets that url then sends a request to it, gets contents, parses contents and returns a map with the results
+    # I want to do the above concurrently. I'd like to create 2,500 requests at the same time.
+    ############################################
 
-    IO.puts "Finished in ... seconds"
+    Logger.info "Finished in ... seconds"
   end
 end
