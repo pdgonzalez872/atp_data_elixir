b13310c22aafd7f854ec4bbe6920b1232bc26735
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-01-30 17:16:40 -0600

    WIP

--- lib/player_page.ex
+++ lib/player_page.ex
@@ -1,5 +1,7 @@
+require IEx
+
 defmodule PlayerPage do
-  def run(url) do
+  def process(url) do
     # result = url
     # |> fetch_data
     # |> parse_html
@@ -7,14 +9,66 @@ defmodule PlayerPage do
     {:ok, url}
   end
 
-  # request to the url
   def fetch_data(url) do
-
+    HTTPoison.start
+    %{body: body} = HTTPoison.get!(url)
+    body
   end
 
   # parses request with library and returns tuple
   # returns  tuple
   def parse_html(html) do
 
+    IEx.pry
+
+    %{
+      ranking: parse_ranking(html),
+      first_name: parse_first_name(html),
+      last_name: parse_last_name(html),
+      country: parse_country(html),
+      birthday: parse_birthday(html),
+      prize_money: parse_career_prize_money(html)
+    }
+
+  end
+
+  def parse_ranking(html) do
+    [{_, [{_, _}, {_, _}], [ranking_dirty]}] = Floki.find(html, ".player-ranking-position .data-number")
+
+    ranking_dirty
+    |> String.trim
+  end
+
+  def parse_first_name(html) do
+    [{_, [{_, _}], [first_name]}] = Floki.find(html, ".player-profile-hero-name .first-name")
+    first_name
+  end
+
+  def parse_last_name(html) do
+    [{_, [{_, _}], [last_name]}] = Floki.find(html, ".player-profile-hero-name .last-name")
+    last_name
+  end
+
+  def parse_country(html) do
+    [{_, [{_, _}], [country]}] = Floki.find(html, ".player-flag-code")
+    country
+  end
+
+  def parse_birthday(html) do
+    [{_, [{_, _}], [birthday_dirty]}] = Floki.find(html, ".table-birthday")
+
+    birthday_clean = birthday_dirty
+    |> String.replace("(", "")
+    |> String.replace(")", "")
+    |> String.replace(".", "-")
+    |> String.trim
+
+    birthday_clean
+  end
+
+  def parse_career_prize_money(html) do
+    {_, [{_, _}, {_, _}, {_, _}], [dirty_prize_money]} = Enum.at(Floki.find(html, ".players-stats-table .stat-value"), -1)
+    dirty_prize_money
+    |> String.trim
   end
 end
---
+++ test/player_page_test.exs
@@ -0,0 +1,38 @@
+require IEx
+
+defmodule PlayerPageTest do
+  use ExUnit.Case
+  doctest PlayerPage
+
+  describe "integration test" do
+    test "works in integration" do
+
+      "/en/players/rafael-nadal/n409/overview"
+      |> PlayerPage.process
+
+      # get a player url
+      # combine it with the base url
+      # send a request to that url
+      # parse the response
+      # return a map with the data and key that includes the string below:
+      # ranking|first_name|last_name|country|birthday|prize_money
+
+      assert 1 == 1
+    end
+  end
+
+  describe "parse_html/1" do
+    test "returns a map with the correct data" do
+      html_string = Path.absname(Path.join(["test", "test_data","roger.htm"]))
+      |> File.read!
+
+      result = PlayerPage.parse_html(html_string)
+      expected = %{
+        birthday: "1981-08-08",
+      }
+
+      assert result == %{}
+
+    end
+  end
+end
