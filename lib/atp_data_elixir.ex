5ca1622336405f64691dc096f96be7d567ecfdc6
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-01-30 18:18:58 -0600

    Implement streams correctly

--- lib/atp_data_elixir.ex
+++ lib/atp_data_elixir.ex
@@ -16,22 +16,14 @@ defmodule AtpDataElixir do
     Logger.info("Fetch Complete, File created")
 
     Logger.info("Fetching player data")
-    # use file here
-
 
     result = Path.join(["test", "test_data","urls.txt"])
     |> Path.absname
     |> File.stream!
     |> Stream.flat_map(&String.split(&1, "\n"))
     |> Stream.filter(fn(x) -> x != "" end)
-    #|> Stream.map(fn(player_url) -> PlayerPage.process_player(player_url) end)
-    # |> Flow.from_enumerable()
-    # |> Enum.to_list()
-
-    IEx.pry
-    # Let's implement flow now.
-
-
+    |> Stream.map(fn(player_url) -> PlayerPage.process_player(player_url) end)
+    |> Enum.to_list()
 
     Logger.info("Finished in #{Date.diff(Date.utc_today, start_time)} seconds")
   end
