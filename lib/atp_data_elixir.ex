7a874d54cf4b44295f67a4c3807733dc0a8008a1
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-01-30 18:22:50 -0600

    Implement flow, but does not look like it is working -> seems that the requests are going sequentially

--- lib/atp_data_elixir.ex
+++ lib/atp_data_elixir.ex
@@ -20,10 +20,17 @@ defmodule AtpDataElixir do
     result = Path.join(["test", "test_data","urls.txt"])
     |> Path.absname
     |> File.stream!
-    |> Stream.flat_map(&String.split(&1, "\n"))
-    |> Stream.filter(fn(x) -> x != "" end)
-    |> Stream.map(fn(player_url) -> PlayerPage.process_player(player_url) end)
+    |> Flow.from_enumerable()
+    |> Flow.flat_map(&String.split(&1, "\n"))
+    |> Flow.filter(fn(x) -> x != "" end)
+    |> Flow.map(fn(player_url) -> PlayerPage.process_player(player_url) end)
     |> Enum.to_list()
+
+
+    # |> Stream.flat_map(&String.split(&1, "\n"))
+    # |> Stream.filter(fn(x) -> x != "" end)
+    # |> Stream.map(fn(player_url) -> PlayerPage.process_player(player_url) end)
+    # |> Enum.to_list()
 
     Logger.info("Finished in #{Date.diff(Date.utc_today, start_time)} seconds")
   end
