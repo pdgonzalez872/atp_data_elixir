bd404cda05c36b128766de1a64071de42c3bf29b
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-02-14 14:40:30 -0600

    Finish v1

--- lib/atp_data_elixir/earnings_aggregator.ex
+++ lib/atp_data_elixir/earnings_aggregator.ex
@@ -7,22 +7,25 @@ defmodule AtpDataElixir.EarningsAggregator do
   # Sorted latest results. Each time we do a fetch it calls this function as well.
 
   def get_latest_earnings_for_players do
-    Player
-    |> Repo.all
-    |> Enum.map(fn(player) ->
-      get_latest_earnings_for_player(player)
-    end)
-    |> Enum.sort_by(fn(result) ->
-      {_, _, amount} = result
-      amount
-    end)
-    |> Enum.reverse()
+    results =
+      Player
+      |> Repo.all()
+      |> Enum.map(fn player ->
+        get_latest_earnings_for_player(player)
+      end)
+      |> Enum.sort_by(fn result ->
+        {_, _, amount} = result
+        amount
+      end)
+      |> Enum.reverse()
+
+    results
   end
 
   def get_latest_earnings_for_player(player) do
     player_data = preload(Player, :earnings) |> Repo.get(player.id)
     latest_earnings = Enum.at(player_data.earnings, -1)
 
-    { :ok, "#{player.first_name} #{player.last_name}", latest_earnings.amount }
+    {:ok, "#{player.first_name} #{player.last_name}", latest_earnings.amount}
   end
 end
