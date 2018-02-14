ba7ab2839f976e921cc57797f8c91050a2081ae8
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-02-14 08:13:39 -0600

    Associations work, play with api in integration test

--- test/lib/acceptance_test.exs
+++ test/lib/acceptance_test.exs
@@ -9,32 +9,24 @@ defmodule AcceptanceTest do
 
   describe "integration test" do
     test "works in integration" do
-
       alias AtpDataElixir.{Repo, Player, Earning}
 
-      %Player{first_name: "Test"} |> Repo.insert
+      {:ok, player} = %Player{first_name: "Test"} |> Repo.insert
 
-      earning = %Earning{amount: 10000}
-      |> Repo.insert
+      %Earning{amount: 10000, player_id: player.id}
+      |> Repo.insert!
 
-      target_player = Repo.get_by(Player, first_name: "Test") |> Repo.preload([:earnings])
+      target_player = Player
+      |> Repo.get_by(first_name: "Test")
+      |> Repo.preload([:earnings])
 
-      assert target_player.earnings == []
-
-      player_changeset = Ecto.Changeset.change(target_player, earnings: [earning])
-
-      # This fails. Here is the error:
-      #   pry(1)> player_changeset = Ecto.Changeset.change(target_player, earnings: [earning])
-      #   #Ecto.Changeset<
-      #     action: nil,
-      #       changes: %{},
-      #       errors: [earnings: {"is invalid", [type: {:array, :map}]}],
-      #       data: #AtpDataElixir.Player<>,
-      #     valid?: false
-      #   >
-
-      refute target_player.earnings == []
       assert Enum.count(target_player.earnings) == 1
+
+      target_earning = target_player.earnings
+      |> Enum.take(-1)
+      |> Enum.at(0)
+
+      assert target_earning.amount == 10000
     end
   end
 end
