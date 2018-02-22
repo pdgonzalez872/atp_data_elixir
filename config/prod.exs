4afea4256123a3717c8ea81f44865e37393f17be
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-02-22 16:59:38 -0600

    Forgot a config in there

--- config/prod.exs
+++ config/prod.exs
@@ -2,15 +2,6 @@ use Mix.Config
 
 config :atp_data_elixir, AtpDataElixirWeb.Endpoint,
   load_from_system_env: true,
-  http: [port: "${PORT}"],
-  check_origin: false,
-  server: true,
-  root: ".",
-  cache_static_manifest: "priv/static/cache_manifest.json"
-
-
-config :atp_data_elixir, AtpDataElixirWeb.Endpoint,
-  load_from_system_env: true,
   # http: [port: {:system, "PORT"}], # Uncomment this line if you are running Phoenix 1.2
   server: true, # Without this line, your app will not start the web server!
   secret_key_base: "${SECRET_KEY_BASE}",
