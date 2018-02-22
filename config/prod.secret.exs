982955c12b4f88c33353f150f65f05cd614fe685
pdgonzalez872 <pdgonzalez872@gmail.com>
2018-02-22 16:41:35 -0600

    Make config changes

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -8,14 +8,3 @@ use Mix.Config
 # file or create a script for recreating it, since it's
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
-config :atp_data_elixir, AtpDataElixirWeb.Endpoint,
-  secret_key_base: "GyxTdjRQDMzbChyYlNTgOAiaX/Vg/1vuBzqICMXHCoBa4C9PGS/2sni9HapiyBcH"
-
-# Configure your database
-config :atp_data_elixir, AtpDataElixir.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "paulo",
-  database: "atp_data_elixir_prod",
-  socket: "/https://www.googleapis.com/sql/v1beta4/projects/atpelixir/instances/atpelixir/databases/postgres/ATPElixir:us-central1:atpelixir/.s.PGSQL.5432",
-  pool_size: 15
