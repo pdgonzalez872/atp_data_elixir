73e6aa8d8eba56bc16d24897cdc32fb9088006a7
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-06-01 18:28:22 -0500

    Refactor: iterate once to gather data

--- lib/atp_data_elixir_web/controllers/page_controller.ex
+++ lib/atp_data_elixir_web/controllers/page_controller.ex
@@ -11,20 +11,11 @@ defmodule AtpDataElixirWeb.PageController do
     # TODO: https://github.com/pdgonzalez872/atp_data_elixir/issues/50
     player_data = EarningsAggregator.get_latest_earnings_for_players()
 
-    # TODO: improve - iterating twice
-    # https://stackoverflow.com/questions/45934946/compare-and-reduce-pairs-in-two-lists
-    chart_labels =
+    {chart_labels, chart_data} =
       player_data
-      |> Enum.map(fn el ->
-        %{ amount:  _, name: name } = el
-        name
-      end)
-
-    chart_data =
-      player_data
-      |> Enum.map(fn(el) ->
-        %{ amount: amount, name: _ } = el;
-        amount
+      |> Enum.reduce({[], []}, fn el, {chart_labels, chart_data} ->
+        %{ amount: amount, name: name } = el
+        {chart_labels ++ [name], chart_data ++ [amount]}
       end)
 
     json(conn, [chart_labels, chart_data])
