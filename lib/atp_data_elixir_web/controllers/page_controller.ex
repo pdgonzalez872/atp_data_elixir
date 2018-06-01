2279b97f71bd71fd7b5785f43b18351e284993c0
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-06-01 18:04:37 -0500

    Add comments to controller

--- lib/atp_data_elixir_web/controllers/page_controller.ex
+++ lib/atp_data_elixir_web/controllers/page_controller.ex
@@ -1,19 +1,31 @@
 defmodule AtpDataElixirWeb.PageController do
   use AtpDataElixirWeb, :controller
 
+  alias AtpDataElixir.{EarningsAggregator}
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def chart_data(conn, _params) do
-    player_data = AtpDataElixir.EarningsAggregator.get_latest_earnings
+    # TODO: https://github.com/pdgonzalez872/atp_data_elixir/issues/50
+    player_data = EarningsAggregator.get_latest_earnings_for_players()
 
     # TODO: improve - iterating twice
-    chart_labels = player_data
-                   |> Enum.map(fn(el) -> %{ "amount" => _, "name" => name } = el; name end)
+    # https://stackoverflow.com/questions/45934946/compare-and-reduce-pairs-in-two-lists
+    chart_labels =
+      player_data
+      |> Enum.map(fn el ->
+        %{ amount:  _, name: name } = el
+        name
+      end)
 
-    chart_data = player_data
-                 |> Enum.map(fn(el) -> %{ "amount" => amount, "name" => _ } = el; amount end)
+    chart_data =
+      player_data
+      |> Enum.map(fn(el) ->
+        %{ amount: amount, name: _ } = el;
+        amount
+      end)
 
     json(conn, [chart_labels, chart_data])
   end
